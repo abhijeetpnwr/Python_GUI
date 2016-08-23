2774fb3af22fd050f1d8967382a259f02ba4d091
abhijeetpnwr <abhijeetpnwr@gmail.com>
2016-08-23 23:16:32 +0530

    2nd type GUI interation using events

--- gridlayout.py
+++ gridlayout.py
@@ -3,36 +3,22 @@
 
 from Tkinter import *
 
+def leftClick(event):
+	print "Left button Clicked"
 
-
-def doBlast():
-	print "Should do Blast stuff"
+def rightClick(event):
+	print "right button clicked"
 
 root = Tk() #A blank constructor , creates a simple window 
 
-Label1 = Label(root,text="Enter Username")
+frame = Frame(root,height=600,width=400)
 
-Label2  = Label(root,text="Enter Password")
+frame.bind("<Button-1>",leftClick)
 
-entry_1 = Entry(root,text="Enter username")
+frame.bind("<Button-3>",rightClick)  #Button-2 is for middle mouse button
 
-entry_2 = Entry(root,text="Enter password")
-
-c = Checkbutton(root,text="Keep me logged in")
-
-button_blast = Button(root,text = "Do Blast",command=doBlast)
-
-Label1.grid(row=0, sticky=E) # Put in  1st row , 1st column  , sticly  makes it ticl to east
-
-Label2.grid(row=1)
-
-entry_1.grid(row=0,column=1)
-
-entry_2.grid(row=1,column=1)
-
-c.grid(columnspan=2)
-
-button_blast.grid(row=3)
+frame.pack()
 
 root.mainloop()
+
 
