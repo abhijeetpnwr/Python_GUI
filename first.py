77138c3e15cec1d367ed3373b589e2044dd89d32
abhijeetpnwr <abhijeetpnwr@gmail.com>
2016-08-23 21:30:07 +0530

    Button added at bottom

--- first.py
+++ first.py
@@ -5,6 +5,16 @@ from Tkinter import *
 
 root = Tk() #A blank constructor , creates a simple window 
 
+topFrame = Frame(root) #Created a Frame
+topFrame.pack() # Pack frame in window
+
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM) # Packing bottom Frame 
+
+button = Button(bottomFrame,text="Sart Blast",fg="purple")
+button.pack()
+
+
 thelabel = Label(root,text="Blast++ Wrapper Tutorial") #created a label
 
 thelabel.pack() # It means just pack all widgest into window , putting widget to root table 
