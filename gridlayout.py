cebb2cd0a7eb06d65d48f09a1396dd6807607c83
abhijeetpnwr <abhijeetpnwr@gmail.com>
2016-08-23 22:55:48 +0530

    functio called on button click

--- gridlayout.py
+++ gridlayout.py
@@ -3,6 +3,11 @@
 
 from Tkinter import *
 
+
+
+def doBlast():
+	print "Should do Blast stuff"
+
 root = Tk() #A blank constructor , creates a simple window 
 
 Label1 = Label(root,text="Enter Username")
@@ -15,6 +20,8 @@ entry_2 = Entry(root,text="Enter password")
 
 c = Checkbutton(root,text="Keep me logged in")
 
+button_blast = Button(root,text = "Do Blast",command=doBlast)
+
 Label1.grid(row=0, sticky=E) # Put in  1st row , 1st column  , sticly  makes it ticl to east
 
 Label2.grid(row=1)
@@ -25,4 +32,7 @@ entry_2.grid(row=1,column=1)
 
 c.grid(columnspan=2)
 
+button_blast.grid(row=3)
+
 root.mainloop()
+
